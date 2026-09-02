139b5bed70f81f932cbd675e2ffb4b83436b953f
rogilmore <rog1@psu.edu>
2026-09-02 08:27:32 -0400

    Update 2026-09-02 slides.

--- R/make_save_ai_image.R
+++
@@ -1,9 +0,0 @@
-make_save_ai_image <- function(prompt = "Nittany Lion",
-                               size = "512x512") {
-  img_url <- openai::create_image(prompt = prompt,
-                       n = 1,
-                       size = size, 
-                       response_format = "url")
-  img_url
-}
-
